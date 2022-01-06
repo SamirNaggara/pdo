31ff8727c5b5dd7f047ce87e68feb05699fbfaae
Sam <samirm.nagg@gmail.com>
2022-01-06 15:15:49 +0100

    ajoute du isset du get id dans detail-voiture

--- detail-voiture.php
+++ detail-voiture.php
@@ -2,3 +2,10 @@
 
 
 // Ici, il y a le code de detail-voiture.php
+
+
+if (isset($_GET["id"]) && !empty($_GET["id"])){
+
+
+    echo "Super, il existe bien un id !<br> Et il est egal a " . $_GET["id"];
+}
